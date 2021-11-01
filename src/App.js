95c4e0b3aa53b33a7dc132b0b136de30f0736a56
Maulik Patel <work.maulikp@gmail.com>
2021-11-01 10:05:18 +0530

    :construction:  added console for env

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const envs = process.env;
+  let verb = Object.keys(envs).reduce(key => (acc, key) => {
+    return acc + ` ${key}: ${envs[key]} \n`
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ENV Test
+          <pre>
+            {verb}
+          </pre>
         </p>
         <a
           className="App-link"
@@ -15,7 +24,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ENV React
         </a>
       </header>
     </div>
